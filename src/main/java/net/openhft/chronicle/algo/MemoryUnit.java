6e4682c33322db45772fe16d25c4a61d4ac6c17b
Peter Lawrey <peter.lawrey@chronicle.software>
2025-11-11 15:01:32 +0000

    Update copyright headers to reflect 2013-2025 and SPDX-License-Identifier: Apache-2.0

--- src/main/java/net/openhft/chronicle/algo/MemoryUnit.java
+++ src/main/java/net/openhft/chronicle/algo/MemoryUnit.java
@@ -1,18 +1,7 @@
 /*
- *     Copyright (C) 2015-2020 chronicle.software
- *
- *     This program is free software: you can redistribute it and/or modify
- *     it under the terms of the GNU Lesser General Public License as published by
- *     the Free Software Foundation, either version 3 of the License.
- *
- *     This program is distributed in the hope that it will be useful,
- *     but WITHOUT ANY WARRANTY; without even the implied warranty of
- *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *     GNU Lesser General Public License for more details.
- *
- *     You should have received a copy of the GNU Lesser General Public License
- *     along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * Copyright 2013-2025 chronicle.software; SPDX-License-Identifier: Apache-2.0
  */
+
 
 /*
  * Based on java.util.concurrent.TimeUnit, which is
@@ -20,7 +9,6 @@
  * Expert Group and released to the public domain, as explained at
  * http://creativecommons.org/publicdomain/zero/1.0/
  */
-
 package net.openhft.chronicle.algo;
 
 import java.util.concurrent.TimeUnit;
